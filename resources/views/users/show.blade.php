39e4d0d879b3321b3494bb813dcb9dafa1b4787f
suguru.asahara <suguru.asahara.820@gmail.com>
2021-05-16 11:41:01 +0000

    user page

---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <aside class="col-sm-4">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">{{ $user->name }}</h3>
+                </div>
+                <div class="card-body">
+                    {{-- ユーザのメールアドレスをもとにGravatarを取得して表示 --}}
+                    <img class="rounded img-fluid" src="{{ Gravatar::get($user->email, ['size' => 500]) }}" alt="">
+                </div>
+            </div>
+        </aside>
+        <div class="col-sm-8">
+        </div>
+    </div>
+@endsection
